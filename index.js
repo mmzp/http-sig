6d2b6d1936109b946440bd7f29010203c63e7535
linzf <mmzp@qq.com>
2019-08-26 16:58:32 +0800

    update: params 支持直接传递字符串；

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const crypto = require('crypto');
 
 module.exports = {
     generate(method, pathInfo, params, secret) {
-        // 1、字典序排序
-        // 2、排序后的参数格式化为 query 格式，形如：key1=value1&key2=value2
-        // 3、拼接成 `${method}\n${pathInfo}\n${query}\n${secret}`
-        // 4、计算拼接后字符串的 MD5 值
+        // 1、params 参数格式化为字符串：
+        //     a. 若参数是一个字典对象，则进行字典序排序，排序后的参数格式化为 query 格式，形如：key1=value1&key2=value2
+        //     b. 若参数是一个字符串，则无需格式化
+        // 2、拼接成 `${method}\n${pathInfo}\n${query}\n${secret}`
+        // 3、计算拼接后字符串的 MD5 值
         if (method) {
             method = method.toUpperCase();
         } else {
@@ -20,12 +21,17 @@ module.exports = {
         if (!secret) {
             secret = '';
         }
-        const keys = Object.keys(params).sort();
-        const segments = [];
-        for (let key of keys) {
-            segments.push(params[key]);
+        let str;
+        if (typeof params === 'string') {
+            str = params;
+        } else {
+            const keys = Object.keys(params).sort();
+            const segments = [];
+            for (let key of keys) {
+                segments.push(params[key]);
+            }
+            str = segments.join('&');
         }
-        const str = segments.join('&');
 
         return crypto
             .createHash('md5')
